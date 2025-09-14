3f7643c71233fd8c57248c219c48e5d9f351da15
Aishwarya Gopalakrishnan <aishwaryag9424@gmail.com>
2025-09-13 17:38:26 -0700

    commit 5

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from utils.model_loader import ModelLoader
+from utils.Model_loader import ModelLoader
 from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import DocumentPortalException
 from model.models import *
